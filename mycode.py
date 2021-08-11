df5fb93d47367f12c912b86e7b6a29abcb4923b4
Tom Cobb <tom.cobb@diamond.ac.uk>
2021-08-11 09:45:14 +0100

    minimal repro

---
+++ mycode.py
@@ -0,0 +1,9 @@
+def add_numbers(a: int, b: int = 0) -> int:
+    """Add two numbers together
+
+    :param a: The first number
+    :param b: The second number
+
+    It's a very useful function
+    """
+    return a + b
